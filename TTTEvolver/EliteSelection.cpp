66f1aaaf6e370436c310cb68585ea0de009e0c6e
dkirkpatrick <dkinventor@live.com>
2016-10-26 02:50:44 -0400

    Fixed selection operator for strategies, added games

--- TTTEvolver/EliteSelection.cpp
+++ TTTEvolver/EliteSelection.cpp
@@ -4,16 +4,31 @@
 */
 
 #include "EliteSelection.h"
+#include "Random.h"
 
-EliteSelector::EliteSelector(double rateSel, int numEval) : Selector(rateSel, numEval){}
+EliteSelector::EliteSelector(double rateSel, int numEval) : Selector(rateSel, numEval){
+	myRef = BoardDictionary();
+	m_winVal = 1.0; 
+	m_lossVal = 0.0; 
+	m_drawVal = 1.0;
+}
+
+EliteSelector::EliteSelector(double rateSel, int numEval, double winVal, double lossVal, double drawVal): Selector(rateSel, numEval), m_lossVal(lossVal), m_winVal(winVal), m_drawVal(drawVal) {
+	myRef = BoardDictionary();
+}
 
 std::vector<Strategy> EliteSelector::createNextGen(std::vector<Strategy> oldGeneration){
 	for(Strategy p: oldGeneration){
 		p.fitness = 0.0; 
-		int StrategySize = p.PlayArray.size(); 
-		for(int i = 0; i < m_numEval; i++){
-			int evalTemp = rand() % StrategySize;
-			p.fitness+=p.PlayArray.at(evalTemp); 
+		for (Strategy p2 : oldGeneration) {
+			double value = play(p, p2); 
+			if (value > 0) {
+				p.fitness += m_winVal; 
+			} else if (value < 0) {
+				p.fitness += m_lossVal;
+			} else {
+				p.fitness += m_drawVal;
+			}
 		}
 	}
 
@@ -45,4 +60,47 @@ std::vector<Strategy> EliteSelector::createNextGen(std::vector<Strategy> oldGene
 	}
 
 	return newGeneration; 
+}
+
+double EliteSelector::play(Strategy s1, Strategy s2) {
+	int playCount = 0;
+	TTTGame myGame = TTTGame(3);
+	
+	// Assign player to brain, opponent
+	int s1Plays = Random::getInt(1, 2);
+	int s2Plays = (s1Plays == 2 ? 1 : 2);
+	bool whoPlays = (s2Plays == 1);
+
+	// Handles the result until a conclusion is reached 
+	while (myGame.gameWon() == 0 && !myGame.gameDraw()) {
+		if (whoPlays) {
+			int otherPlay = getPlay(s2, myGame, s2Plays);
+			myGame.play(otherPlay, s2Plays);
+			whoPlays = false;
+		}
+		else {
+			int myPlay = getPlay(s1, myGame, s1Plays);
+			myGame.play(myPlay, s1Plays);
+			whoPlays = true;
+		}
+	}
+
+	// Outputs based on game results 
+	if (myGame.gameDraw()) {
+		return 0;
+	}
+	else if (myGame.gameWon() == s1Plays) {
+		return 1;
+	}
+	else {
+		return -1;
+	}
+}
+
+int EliteSelector::getPlay(Strategy s, TTTGame mGame, int whichPlayer)
+{
+	std::vector<int> lookup = myRef.dict().at(mGame.base3Board()); 
+	std::vector<int> permute = mGame.getInversePermutation(myRef.getMask(lookup.at(1))); 
+	int offset = (whichPlayer == 1) ? 0 : 1582; 
+	return permute.at(s.PlayArray.at(lookup.at(2)+offset));
 }
--- TTTEvolver/EliteSelection.h
+++ TTTEvolver/EliteSelection.h
@@ -2,12 +2,22 @@
 #define EliteSelection_H_
 
 #include "Selector.h"
-
+#include "TTTEngine.h"
+#include "BoardDictionary.h"
 class EliteSelector : public Selector{
 public:
 	EliteSelector(double rateSel, int numEval);
-	
+	EliteSelector(double rateSel, int numEval, double winVal, double lossVal, double drawVal);
 	std::vector<Strategy> createNextGen(std::vector<Strategy> oldGeneration); 
+
+	double play(Strategy s1, Strategy s2); 
+	int getPlay(Strategy s, TTTGame mGame, int whichPlayer);
+
+private:
+	BoardDictionary myRef; 
+	double m_winVal; 
+	double m_lossVal; 
+	double m_drawVal; 
 };
 
 
